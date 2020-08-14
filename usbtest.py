1a4bf79b4e1f4ced45b9f4d9f10848bcaae55294
Tim Bourguignon <timothee.bourguignon@mathema.de>
2020-08-14 15:49:59 +0200

    First working version

--- usbtest.py
+++ usbtest.py
@@ -14,11 +14,12 @@ def main(argv):
         currentDir = pathlib.Path(os.path.curdir)
         dummyFile = currentDir.joinpath(".dummy")
         if not dummyFile.exists():
-            bar = Bar('Creating Dummy File', max=size)
+            bar = Bar('Creating %s Mb dummy file' %(size/1000), max=size)
             with dummyFile.open(mode='w') as d:
                 for _ in range(size):
                     d.writelines("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feu")
                     bar.next()
+                print("\n") # To prevent the next string to be added with no linebreak
         return dummyFile
 
     try: # Verify Arguments
@@ -39,18 +40,16 @@ def main(argv):
     source = str(dummyFile.absolute())
     target = str(drive.absolute())
 
+    print("Testing...")
     tick = time.perf_counter()
-    print("Copying file...")
     shutil.copy(source, target)
     elapsedTime = time.perf_counter() - tick
-    speed = kiloByteSize/elapsedTime
+    speed = round(kiloByteSize/elapsedTime/1000, 2)
 
-    dummyFile.unlink()
+    #dummyFile.unlink()
 
-    print("Filesize: " + str(kiloByteSize) + " Kb")
-    print("Elapsedtime: " + str(elapsedTime) + " seconds") 
-
-    print ("Your disk drive speed is %s Kb/sec" %speed)
+    print ("Elapsedtime: %s seconds" %elapsedTime) 
+    print ("Speed: %s Mb/sec" %speed)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
